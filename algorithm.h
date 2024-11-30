b0a6ac6fd6c9132195bb2ffc8895b65050a5e1dc
Soumyadip Mandal <24je0699@iitism.ac.in>
2024-11-30 17:58:19 +0530

    Binary Search

--- algorithm.h
+++ algorithm.h
@@ -28,3 +28,64 @@ int linearSearch(int arr[], int length, int key)
 			return i;
 	}
 }
+
+int binarySearchLeft(int arr[], int low, int high, int key)
+{
+	// Calculating the middle value
+	int mid = low + (high - low) / 2;
+
+	if (arr[mid] == key)
+	{
+		// arr[low - 1] is out of range
+		if (mid == low)
+			return mid;
+		// Checking if any other element in the left is equal to the key
+		else if (arr[mid - 1] == key)
+			return binarySearchLeft(arr, low, mid, key);
+		else
+			return mid;
+	}
+
+	// If key is not found
+	else if (mid == low || mid == high)
+		return -1;
+
+	else if (arr[mid] > key)
+		return binarySearchLeft(arr, low, mid, key);
+	else
+		return binarySearchLeft(arr, mid, high, key);
+}
+
+int binarySearchRight(int arr[], int low, int high, int key)
+{
+	// Calculating the middle value
+	int mid = low + (high - low) / 2;
+
+	if (arr[mid] == key)
+	{
+		// arr[high + 1] is out of range
+		if (mid == high)
+			return mid;
+		// Checking if any other element in right is equal to the key
+		else if (arr[mid + 1] == key)
+			// if low = high - 1, then
+			// mid = (high - 1) - (high - (high - 1)) / 2
+			// mid = high - 1 - (high - high + 1) / 2
+			// mid = high - 1 - 1/2 = high - 1
+			// mid = low
+			// To fix this issue, mid + 1 is taken instead of mid
+			// as done in binarySearchLeft
+			return binarySearchRight(arr, mid + 1, high, key);
+		else
+			return mid;
+	}
+
+	// If key is not found
+	else if (mid == low || mid == high)
+		return -1;
+
+	else if (arr[mid] > key)
+		return binarySearchRight(arr, low, mid, key);
+	else
+		return binarySearchRight(arr, mid, high, key);
+}
