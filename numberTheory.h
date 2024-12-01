9485b92e018eef102f43d1c6ac8fc2e29760ba23
Soumyadip Mandal <24je0699@iitism.ac.in>
2024-12-02 00:09:38 +0530

    Horner's Method

--- numberTheory.h
+++ numberTheory.h
@@ -19,3 +19,17 @@ int gcd(int a, int b)
 	else
 		return gcd(b, a % b);
 }
+
+int horner(int arr[], int x, int length)
+{
+	// If an array [a0, a1, a2, ..., an] is given, then
+	// a0 + a1 * x + a2 * x^2 + ... + an * x^n is returned
+	// a0 + x * (a1 + x* (a2 + ... + x * (an-1 + x * an)))
+
+	int result = 0;
+
+	for (int i = length - 1; i >= 0; i--)
+		result = arr[i] + result * x;
+
+	return result;
+}
