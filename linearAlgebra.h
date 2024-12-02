f8a82c2c85d68e0c5d81cd70b38d6d1ba4c75a55
Soumyadip Mandal <24je0699@iitism.ac.in>
2024-12-02 21:40:08 +0530

    Gauss Jordan Elimination

--- linearAlgebra.h
+++ linearAlgebra.h
@@ -4,7 +4,8 @@
 
 void gaussianElimination(int size, float coefficient[size][size], float augmented[size])
 {
-	// Solve a linear equation using Gaussian Elimination
+	// Solve a linear equation using Gaussian Elimination.
+	// Coefficient matrix is of size n x n and augmented vector is of size n
 
 	// Converts the matrix to row echelon form
 	for (int i = 0; i < size; i++)
@@ -33,17 +34,18 @@ void gaussianElimination(int size, float coefficient[size][size], float augmente
 
 		for (int j = i + 1; j < size; j++)
 		{
-			// If alpha is zero, then there will be no changes
+			// If multiplyingFactor is zero, then there will be no changes
 			if (coefficient[j][i] == 0)
 				continue;
-			float alpha = coefficient[j][i] / coefficient[i][i];
+			float multiplyingFactor = coefficient[j][i] / coefficient[i][i];
 			coefficient[j][i] = 0;
 			for (int k = i + 1; k < size; k++)
-				coefficient[j][k] -= coefficient[i][k] * alpha;
-			augmented[j] -= augmented[i] * alpha;
+				coefficient[j][k] -= coefficient[i][k] * multiplyingFactor;
+			augmented[j] -= augmented[i] * multiplyingFactor;
 		}
 	}
 
+	// Calculating the result by back substitution
 	float sum;
 	for (int i = size - 1; i >= 0; i--)
 	{
@@ -51,5 +53,58 @@ void gaussianElimination(int size, float coefficient[size][size], float augmente
 		for (int j = i + 1; j < size; j++)
 			sum += coefficient[i][j] * augmented[j];
 		augmented[i] = (augmented[i] - sum) / coefficient[i][i];
+	}
+}
+
+void gaussJordanElimination(int size, float coefficient[size][size], float augmented[size])
+{
+	// Solve a linear equation using Gaussian - Jordan Elimination.
+	// Coefficient matrix is of size n x n and augmented vector is of size n
+
+	// Converts the matrix to row echelon form
+	for (int i = 0; i < size; i++)
+	{
+		// For handling zero divisions
+		if (coefficient[i][i] == 0)
+		{
+			for (int j = i + 1; j < size; j++)
+			{
+				if (coefficient[j][i])
+				{
+					float temp;
+					for (int k = 0; k < size; k++)
+					{
+						temp = coefficient[i][k];
+						coefficient[i][k] = coefficient[j][k];
+						coefficient[j][k] = temp;
+					}
+					temp = augmented[i];
+					augmented[i] = augmented[j];
+					augmented[j] = temp;
+					break;
+				}
+			}
+		}
+
+		float dividingFactor = coefficient[i][i];
+		coefficient[i][i] = 1;
+
+		// Setting the pivot to 1
+		for (int j = i + 1; j < size; j++)
+			coefficient[i][j] /= dividingFactor;
+		augmented[i] /= dividingFactor;
+
+		// Computing the result
+		float multiplyingFactor;
+		for (int k = 0; k < size; k++)
+		{
+			if (k != i)
+			{
+				multiplyingFactor = coefficient[k][i];
+				for (int j = i; j < size; j++)
+					coefficient[k][j] -= multiplyingFactor * coefficient[i][j];
+				augmented[k] -= multiplyingFactor * augmented[i];
+			}
+		}
 	}
 }
