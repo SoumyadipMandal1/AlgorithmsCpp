9c3ae264b7ac1e4e10d7472ab743ff540bd3a765
Soumyadip Mandal <24je0699@iitism.ac.in>
2024-12-02 19:58:25 +0530

    Bug fixes and optimization of Gaussian Elemination

--- linearAlgebra.h
+++ linearAlgebra.h
@@ -9,8 +9,33 @@ void gaussianElimination(int size, float coefficient[size][size], float augmente
 	// Converts the matrix to row echelon form
 	for (int i = 0; i < size; i++)
 	{
+		// For handling zero divisions
+		if (coefficient[i][i] == 0)
+		{
+			for (int j = i + 1; j < size; j++)
+			{
+				if (coefficient[j][i])
+				{
+					float temp;
+					for (int k = 0; k < size; k++)
+					{
+						temp = coefficient[i][k];
+						coefficient[i][k] = coefficient[j][k];
+						coefficient[j][k] = temp;
+					}
+					temp = augmented[i];
+					augmented[i] = augmented[j];
+					augmented[j] = temp;
+					break;
+				}
+			}
+		}
+
 		for (int j = i + 1; j < size; j++)
 		{
+			// If alpha is zero, then there will be no changes
+			if (coefficient[j][i] == 0)
+				continue;
 			float alpha = coefficient[j][i] / coefficient[i][i];
 			coefficient[j][i] = 0;
 			for (int k = i + 1; k < size; k++)
