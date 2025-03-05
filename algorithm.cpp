b7db253fe1d0f584599e50c135c6184cfc09b02f
SoumyadipMandal1 <24je0699@iitism.ac.in>
2025-03-05 19:42:39 +0530

    Converted algorithm file from C to C++

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,15 +1,17 @@
-void swap(int* a, int* b)
+#include <vector>
+
+void swap(int& a, int& b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+	int temp = a;
+	a = b;
+	b = temp;
 }
 
-int linear(int arr[], int length, int key)
+int linear(std::vector<int> array, int length, int key)
 {
 	for (int i = 0; i < length; i++)
 	{
-		if (arr[i] == key)
+		if (array[i] == key)
 			return i;
 	}
 
@@ -17,19 +19,19 @@ int linear(int arr[], int length, int key)
 	return -1;
 }
 
-int binaryLeft(int arr[], int low, int high, int key)
+int binaryLeft(std::vector<int> array, int low, int high, int key)
 {
 	// Calculating the middle value
 	int mid = low + (high - low) / 2;
 
-	if (arr[mid] == key)
+	if (array[mid] == key)
 	{
-		// arr[low - 1] is out of range
+		// array[low - 1] is out of range
 		if (mid == low)
 			return mid;
 		// Checking if any other element in the left is equal to the key
-		else if (arr[mid - 1] == key)
-			return binaryLeft(arr, low, mid, key);
+		else if (array[mid - 1] == key)
+			return binaryLeft(array, low, mid, key);
 		else
 			return mid;
 	}
@@ -38,24 +40,24 @@ int binaryLeft(int arr[], int low, int high, int key)
 	else if (mid == low || mid == high)
 		return -1;
 
-	else if (arr[mid] > key)
-		return binaryLeft(arr, low, mid, key);
+	else if (array[mid] > key)
+		return binaryLeft(array, low, mid, key);
 	else
-		return binaryLeft(arr, mid, high, key);
+		return binaryLeft(array, mid, high, key);
 }
 
-int binaryRight(int arr[], int low, int high, int key)
+int binaryRight(std::vector<int> array, int low, int high, int key)
 {
 	// Calculating the middle value
 	int mid = low + (high - low) / 2;
 
-	if (arr[mid] == key)
+	if (array[mid] == key)
 	{
-		// arr[high + 1] is out of range
+		// array[high + 1] is out of range
 		if (mid == high)
 			return mid;
 		// Checking if any other element in right is equal to the key
-		else if (arr[mid + 1] == key)
+		else if (array[mid + 1] == key)
 			// if low = high - 1, then
 			// mid = (high - 1) - (high - (high - 1)) / 2
 			// mid = high - 1 - (high - high + 1) / 2
@@ -63,7 +65,7 @@ int binaryRight(int arr[], int low, int high, int key)
 			// mid = low
 			// To fix this issue, mid + 1 is taken instead of mid
 			// as done in binarySearchLeft
-			return binaryRight(arr, mid + 1, high, key);
+			return binaryRight(array, mid + 1, high, key);
 		else
 			return mid;
 	}
@@ -72,134 +74,146 @@ int binaryRight(int arr[], int low, int high, int key)
 	else if (mid == low || mid == high)
 		return -1;
 
-	else if (arr[mid] > key)
-		return binaryRight(arr, low, mid, key);
+	else if (array[mid] > key)
+		return binaryRight(array, low, mid, key);
 	else
-		return binaryRight(arr, mid, high, key);
+		return binaryRight(array, mid, high, key);
 }
 
-int min(int arr[], int length)
+int min(std::vector<int> array, int length)
 {
-	int min = arr[0];
+	int min = array[0];
 	for (int i = 1; i < length; i++)
 	{
-		if (min > arr[i])
-			min = arr[i];
+		if (min > array[i])
+			min = array[i];
 	}
 	return min;
 }
 
-int max(int arr[], int length)
+int max(std::vector<int> array, int length)
 {
-	int max = arr[0];
+	int max = array[0];
 	for (int i = 1; i < length; i++)
 	{
-		if (max < arr[i])
-			max = arr[i];
+		if (max < array[i])
+			max = array[i];
 	}
 	return max;
 }
 
-void bubbleSort(int arr[], int length)
+std::vector<int> bubbleSort(std::vector<int> array, int length, bool reverse = false)
 {
-	for (int i = 0; i < length; i++)
+	if (reverse)
 	{
-		for (int j = i + 1; j < length; j++)
+		for (int i = 0; i < length; i++)
 		{
-			if (arr[i] > arr[j])
-				swap(&arr[i], &arr[j]);
+			for (int j = i + 1; j < length; j++)
+			{
+				if (array[i] < array[j])
+					swap(array[i], array[j]);
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = i + 1; j < length; j++)
+			{
+				if (array[i] > array[j])
+					swap(array[i], array[j]);
+			}
 		}
 	}
 
+	return array;
 }
 
-void bubbleSortReverse(int arr[], int length)
-{
-	for (int i = 0; i < length; i++)
-	{
-		for (int j = i + 1; j < length; j++)
-		{
-			if (arr[i] < arr[j])
-				swap(&arr[i], &arr[j]);
-		}
-	}
-}
-
-void insertionSort(int arr[], int length)
+std::vector<int> insertionSort(std::vector<int> array, int length, bool reverse = false)
 {
 	int key, j; // For Loop optimization
-	for (int i = 1; i < length; i++)
-	{
-		key = arr[i];
-		j = i - 1;
-		while (j >= 0 && arr[j] > key)
-		{
-			arr[j + 1] = arr[j];
-			j--;
-		}
-		arr[j + 1] = key;
-	}
-}
 
-void insertionSortReverse(int arr[], int length)
-{
-	int key, j; // For loop optimization
-	for (int i = 1; i < length; i++)
+	if (reverse)
 	{
-		key = arr[i];
-		j = i - 1;
-		while (j >= 0 && arr[j] < key)
+		int key, j; // For Loop optimization
+		for (int i = 1; i < length; i++)
 		{
-			arr[j + 1] = arr[j];
-			j--;
-		}
-		arr[j + 1] = key;
-	}
-}
-
-void selectionSort(int arr[], int length)
-{
-	int min, minpos; // For loop optimization
-	for (int i = 0; i < length - 1; i++)
-	{
-		min = arr[i];
-		minpos = i;
-		for (int j = i + 1; j < length; j++)
-		{
-			if (min > arr[j])
+			key = array[i];
+			j = i - 1;
+			while (j >= 0 && array[j] < key)
 			{
-				min = arr[j];
-				minpos = j;
+				array[j + 1] = array[j];
+				j--;
 			}
+			array[j + 1] = key;
 		}
-		swap(&arr[i], &arr[minpos]);
 	}
-}
-
-void selectionSortReverse(int arr[], int length)
-{
-	int max, maxpos; // For loop optimization
-	for (int i = 0; i < length - 1; i++)
+	else
 	{
-		max = arr[i];
-		maxpos = i;
-		for (int j = i + 1; j < length; j++)
+		for (int i = 1; i < length; i++)
 		{
-			if (max > arr[j])
+			key = array[i];
+			j = i - 1;
+			while (j >= 0 && array[j] > key)
 			{
-				max = arr[j];
-				maxpos = j;
+				array[j + 1] = array[j];
+				j--;
 			}
+			array[j + 1] = key;
 		}
-		swap(&arr[i], &arr[maxpos]);
 	}
+
+	return array;
 }
 
-void merge(int arr[], int left, int mid, int right)
+std::vector<int> selectionSort(std::vector<int> array, int length, bool reverse = false)
+{
+	if (reverse)
+	{
+		int min, minpos; // For loop optimization
+		for (int i = 0; i < length - 1; i++)
+		{
+			min = array[i];
+			minpos = i;
+			for (int j = i + 1; j < length; j++)
+			{
+				if (min < array[j])
+				{
+					min = array[j];
+					minpos = j;
+				}
+			}
+			swap(array[i], array[minpos]);
+		}
+	}
+	else
+	{
+		int max, minpos; // For loop optimization
+		for (int i = 0; i < length - 1; i++)
+		{
+			max = array[i];
+			minpos = i;
+			for (int j = i + 1; j < length; j++)
+			{
+				if (max > array[j])
+				{
+					max = array[j];
+					minpos = j;
+				}
+			}
+			swap(array[i], array[minpos]);
+		}
+	}
+
+	return array;
+}
+
+void merge(std::vector<int>& array, int left, int mid, int right, bool reverse = false)
 {
 	// Creating two arrays
-	// One array contains form arr[left] to arr[middle]
-	// Other from arr[middle + 1] to arr[right]
+	// One array contains form array[left] to array[middle]
+	// Other from array[middle + 1] to array[right]
 
 	// Length of first array
 	int leftLength = mid - left + 1;
@@ -210,14 +224,14 @@ void merge(int arr[], int left, int mid, int right)
 	//       = right - middle
 	int rightLength = right - mid;
 
-	int leftarr[leftLength];
-	int rightarr[rightLength];
+	int leftarray[leftLength];
+	int rightarray[rightLength];
 
 	for (int i = 0; i < leftLength; i++)
-		leftarr[i]  = arr[left + i];
+		leftarray[i]  = array[left + i];
 
 	for (int i = 0; i < rightLength; i++)
-		rightarr[i] = arr[mid + i + 1];
+		rightarray[i] = array[mid + i + 1];
 
 	// Merging
 
@@ -227,17 +241,35 @@ void merge(int arr[], int left, int mid, int right)
 
 	while (leftpos < leftLength && rightpos < rightLength)
 	{
-		if (leftarr[leftpos] < rightarr[rightpos])
+		if (reverse)
 		{
-			arr[mainpos] = leftarr[leftpos];
-			leftpos++;
-			mainpos++;
+			if (leftarray[leftpos] > rightarray[rightpos])
+			{
+				array[mainpos] = leftarray[leftpos];
+				leftpos++;
+				mainpos++;
+			}
+			else
+			{
+				array[mainpos] = rightarray[rightpos];
+				rightpos++;
+				mainpos++;
+			}
 		}
 		else
 		{
-			arr[mainpos] = rightarr[rightpos];
-			rightpos++;
-			mainpos++;
+			if (leftarray[leftpos] < rightarray[rightpos])
+			{
+				array[mainpos] = leftarray[leftpos];
+				leftpos++;
+				mainpos++;
+			}
+			else
+			{
+				array[mainpos] = rightarray[rightpos];
+				rightpos++;
+				mainpos++;
+			}
 		}
 	}
 
@@ -245,100 +277,39 @@ void merge(int arr[], int left, int mid, int right)
 
 	while (leftpos < leftLength)
 	{
-		arr[mainpos] = leftarr[leftpos];
+		array[mainpos] = leftarray[leftpos];
 		leftpos++;
 		mainpos++;
 	}
 
 	while(rightpos < rightLength)
 	{
-		arr[mainpos] = rightarr[rightpos];
+		array[mainpos] = rightarray[rightpos];
 		rightpos++;
 		mainpos++;
 	}
 }
 
-void mergeSort(int arr[], int left, int right)
+void mergeSort(std::vector<int>& array, int left, int right, bool reverse = false)
 {
-	if (left < right)
+	if (reverse)
 	{
-		int mid = left + (right - left) / 2;
-		mergeSort(arr, left, mid);
-		mergeSort(arr, mid + 1, right);
-		merge(arr, left, mid, right);
-	}
-}
-
-void mergeReverse(int arr[], int left, int mid, int right)
-{
-	// Creating two arrays
-	// One array contains form arr[left] to arr[middle]
-	// Other from arr[middle + 1] to arr[right]
-
-	// Length of first array
-	int leftLength = mid - left + 1;
-
-	// Length of second array
-	// right - (middle + 1) + 1
-	//       = right - middle - 1 + 1
-	//       = right - middle
-	int rightLength = right - mid;
-
-	int leftarr[leftLength];
-	int rightarr[rightLength];
-
-	for (int i = 0; i < leftLength; i++)
-		leftarr[i]  = arr[left + i];
-
-	for (int i = 0; i < rightLength; i++)
-		rightarr[i] = arr[mid + i + 1];
-
-	// Merging
-
-	int leftpos  = 0;    // Keeps track of left array
-	int rightpos = 0;    // Keeps track of right array
-	int mainpos  = left; // Keeps track of main array
-
-	while (leftpos < leftLength && rightpos < rightLength)
-	{
-		if (leftarr[leftpos] > rightarr[rightpos])
+		if (left < right)
 		{
-			arr[mainpos] = leftarr[leftpos];
-			leftpos++;
-			mainpos++;
-		}
-		else
-		{
-			arr[mainpos] = rightarr[rightpos];
-			rightpos++;
-			mainpos++;
+			int mid = left + (right - left) / 2;
+			mergeSort(array, left, mid, reverse = true);
+			mergeSort(array, mid + 1, right, true);
+			merge(array, left, mid, right, true);
 		}
 	}
-
-	// Adding remaining elements
-
-	while (leftpos < leftLength)
+	else
 	{
-		arr[mainpos] = leftarr[leftpos];
-		leftpos++;
-		mainpos++;
-	}
-
-	while(rightpos < rightLength)
-	{
-		arr[mainpos] = rightarr[rightpos];
-		rightpos++;
-		mainpos++;
-	}
-}
-
-void mergeSortReverse(int arr[], int left, int right)
-{
-	if (left < right)
-	{
-		int mid = left + (right - left) / 2;
-		mergeSortReverse(arr, left, mid);
-		mergeSortReverse(arr, mid + 1, right);
-		mergeReverse(arr, left, mid, right);
+		if (left < right)
+		{
+			int mid = left + (right - left) / 2;
+			mergeSort(array, left, mid);
+			mergeSort(array, mid + 1, right);
+			merge(array, left, mid, right);
+		}
 	}
 }
---
+++ algorithm.hpp
@@ -0,0 +1,30 @@
+#ifndef ALGORITHM_HPP
+#define ALGORITHM_HPP
+
+#include <vector>
+
+void swap(int&, int&);
+
+int linear(std::vector<int>, int, int);
+
+int binaryLeft(std::vector<int>, int, int, int);
+
+int binaryRight(std::vector<int>, int, int, int);
+
+int min(std::vector<int>, int);
+
+int max(std::vector<int>, int);
+
+void bubbleSort(std::vector<int>, int);
+
+std::vector<int> bubbleSort(std::vector<int>, int, bool);
+
+std::vector<int> insertionSort(std::vector<int>, int, bool);
+
+std::vector<int> selectionSort(std::vector<int>, int, bool);
+
+void merge(std::vector<int>&, int, int, int, bool);
+
+void mergeSort(std::vector<int>&, int, int, int, bool);
+
+#endif
