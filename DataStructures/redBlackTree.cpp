e8b30a808ba23b3022775bda0ed5ce678a80d886
SoumyadipMandal1 <24je0699@iitism.ac.in>
2026-03-21 15:43:43 +0530

    Error message removed in rotations of red-black tree because normal compilation also gives error in this case

--- DataStructures/redBlackTree.cpp
+++ DataStructures/redBlackTree.cpp
@@ -1,12 +1,8 @@
 #include "redBlackTree.hpp"
-#include <stdexcept>
 
 redBlackTreeNode *leftRotateRedBlackTree(redBlackTreeNode *node)
 {
     redBlackTreeNode *rightSubTree = node->right;
-
-    if (rightSubTree == nullptr)
-        throw std::logic_error("Left rotation cannot be performed on a node having whose right sub-tree is null");
 
     // Left rotation
     node->right = rightSubTree->left;
@@ -18,9 +14,6 @@ redBlackTreeNode *leftRotateRedBlackTree(redBlackTreeNode *node)
 redBlackTreeNode *rightRotateRedBlackTree(redBlackTreeNode *node)
 {
     redBlackTreeNode *leftSubTree = node->left;
-
-    if (leftSubTree == nullptr)
-        throw std::logic_error("Right rotation cannot be performed on a node whose left sub-tree is null");
 
     // Right rotation
     node->left = leftSubTree->right;
